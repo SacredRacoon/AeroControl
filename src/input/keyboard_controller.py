cf5a5698332f19ef5981b121e5a488edad07d39e
Skriabin Aleksei <Sjoa482@gmail.com>
2026-09-14 22:08:15 +0800

    Refactor: update input handling in keyboard and mouse controllers, improve structure and error logging

--- src/input/keyboard_controller.py
+++ src/input/keyboard_controller.py
@@ -1,30 +1,39 @@
 import ctypes
 import logging
+from ctypes import wintypes
 
 logger = logging.getLogger(__name__)
 
 SendInput = ctypes.windll.user32.SendInput
-PUL = ctypes.POINTER(ctypes.c_ulong)
+SendInput.argtypes = [wintypes.UINT, ctypes.c_void_p, ctypes.c_int]
+SendInput.restype = wintypes.UINT
 
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong), ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class InputUnion(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput)]
-
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong), ("ii", InputUnion)]
-
+INPUT_KEYBOARD = 1
 KEYEVENT_KEYDOWN = 0x0000
 KEYEVENT_KEYUP = 0x0002
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = [("wWk", wintypes.WORD),
+                ("wScan", wintypes.WORD),
+                ("dwFlags", wintypes.DWORD),
+                ("time", wintypes.DWORD),
+                ("dwExtraInfo", ctypes.c_void_p)
+                ]
+class _INPUTUNION(ctypes.Union):
+    _fields_ = [("ki", KEYBDINPUT)]
+
+class INPUT(ctypes.Structure):
+    _fields_ = [("type", wintypes.DWORD), 
+                ("union", _INPUTUNION)
+                ]
+
+
 
 VK_MAP = {
     'E': 0x45, 'R': 0x52, 'Q': 0x51, 'F': 0x46, 'C': 0x43,
     'SPACE': 0x20, 'SHIFT': 0xA0, 'CTRL': 0xA2, 'ALT': 0xA4,
     '1': 0x31, '2': 0x32, '3': 0x33, '4': 0x34, '5': 0x35,
-    'TAB': 0x09, 'ENTER': 0x0D, 'ESC': 0x1B 
+    'TAB': 0x09, 'ENTER': 0x0D, 'ESC': 0x1B , 'W': 0x57, 'A': 0x41, 'S': 0x53, 'D': 0x44
 }
 
 class KeyboardController:
@@ -35,53 +44,49 @@ class KeyboardController:
     def press_key(self, key_str: str):
         key_name = str(key_str).upper().strip()
         vk_code = VK_MAP.get(key_name)
-
-        if not vk_code:
-            logger.warning(f"Unknown key {key_str}")
+        if not vk_code or vk_code in self.pressed_keys:
             return
 
-        if vk_code in self.pressed_keys:
-            return
+        ii = _INPUTUNION()
+        ii.ki = KEYBDINPUT(vk_code, 0, KEYEVENT_KEYDOWN, 0, ctypes.c_void_p(0))
+        command = INPUT(INPUT_KEYBOARD,ii)
 
-        logger.debug(f"Key pressed {key_str} (0x{vk_code:02X})")
-
-        ii = InputUnion()
-        ii.ki = KeyBdInput(vk_code, 0, KEYEVENT_KEYDOWN, 0, ctypes.pointer(ctypes.c_ulong(0)))
-        command = Input(ctypes.c_ulong(1),ii)
-        SendInput(1,ctypes.pointer(command), ctypes.sizeof(command))
-        result = SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
+        result = SendInput(1, ctypes.byref(command), ctypes.sizeof(command))
         if result == 0:
-            logger.error("SendInput keydown failed")
-        self.pressed_keys.add(vk_code)
+            err = ctypes.get_last_error()
+            logger.error(f"SendInput keydown failed {key_name} winerr {err}")
+        else:
+            self.pressed_keys.add(vk_code)
+            logger.debug(f"Key pressed {key_name}")
 
     def release_key(self, key_str: str):
         key_name = str(key_str).upper().strip()
-        vk_code = VK_MAP.get(key_str.upper())
-
+        vk_code = VK_MAP.get(key_name)
         if vk_code is None or vk_code not in self.pressed_keys:
             return
 
-        logger.debug(f"Key released {key_name}")
+        ii = _INPUTUNION()
+        ii.ki = KEYBDINPUT(vk_code, 0, KEYEVENT_KEYUP, 0, ctypes.c_void_p(0))
+        command = INPUT(INPUT_KEYBOARD,ii)
 
-        ii = InputUnion()
-        ii.ki = KeyBdInput(vk_code, 0, KEYEVENT_KEYUP, 0, ctypes.pointer(ctypes.c_ulong(0)))
-        command = Input(ctypes.c_ulong(1), ii)
-        SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
-        result = SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
-
+        result = SendInput(1, ctypes.byref(command), ctypes.sizeof(command))
         if result == 0:
-            logger.error("SendInput keyup failed")
-        self.pressed_keys.remove(vk_code)
+            err = ctypes.get_last_error()
+            logger.error(f"SendInput keydup failed {key_name} winerr {err}")
+        else:
+            self.pressed_keys.remove(vk_code)
+            logger.debug(f"Key released {key_name}")
 
     def release_all(self):
         if not self.pressed_keys:
             return
 
         logger.info("All keys released")
+
         for vk_code in list(self.pressed_keys):
-            ii = InputUnion()
-            ii.ki = KeyBdInput(vk_code, 0, KEYEVENT_KEYUP, 0, ctypes.pointer(ctypes.c_ulong(0)))
-            command = Input(ctypes.c_ulong(1), ii)
-            SendInput(1,ctypes.pointer(command), ctypes.sizeof(command))
+            ii = _INPUTUNION()
+            ii.ki = KEYBDINPUT(vk_code, 0, KEYEVENT_KEYUP, 0, ctypes.pointer(ctypes.c_void_p(0)))
+            command = INPUT(INPUT_KEYBOARD.c_ulong(1), ii)
+            SendInput(1,ctypes.byref(command), ctypes.sizeof(command))
 
         self.pressed_keys.clear()
--- src/input/mouse_controller.py
+++ src/input/mouse_controller.py
@@ -1,25 +1,36 @@
 import ctypes
 import logging
+from ctypes import wintypes
 
 logger = logging.getLogger(__name__)
 
 SendInput = ctypes.windll.user32.SendInput
-PUL = ctypes.POINTER(ctypes.c_ulong)
+SendInput.argtypes = [wintypes.UINT, ctypes.c_void_p, ctypes.c_int]
+SendInput.restype = wintypes.UINT
 
-class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long), ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong), ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong), ("dwExtraInfo", PUL)]
-
-class InputUnion(ctypes.Union):
-    _fields_ = [("mi", MouseInput)]
-
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong), ("ii", InputUnion)]
-
+INPUT_MOUSE = 0
 MOUSEEVENTF_MOVE = 0x0001
 MOUSEEVENTF_LEFTDOWN = 0x0002
 MOUSEEVENTF_LEFTUP = 0x0004
+MOUSEEVENTF_RIGHTDOWN = 0x0008
+MOUSEEVENTF_RIGHTUP = 0x0010
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = [("dx",wintypes.LONG), 
+                ("dy", wintypes.LONG),
+                ("mouseData", wintypes.DWORD), 
+                ("dwFlags", wintypes.DWORD),
+                ("time", wintypes.DWORD), 
+                ("dwExtraInfo", ctypes.c_void_p)
+                ]
+
+class _INPUTUNION(ctypes.Union):
+    _fields_ = [("mi", MOUSEINPUT)]
+
+class INPUT(ctypes.Structure):
+    _fields_ = [("type", wintypes.DWORD), 
+                ("union", _INPUTUNION)
+                ]
 
 class MouseController:
     def __init__(self, sensitivity: float):
@@ -34,20 +45,28 @@ class MouseController:
         dx_int = int(dx / self.sensitivity)
         dy_int = int(dy / self.sensitivity)
 
-        ii = InputUnion()
-        ii.mi = MouseInput(dx_int, dy_int, 0, MOUSEEVENTF_MOVE, 0, ctypes.pointer(ctypes.c_ulong(0)))
-        command = Input(ctypes.c_ulong(0), ii)
-        SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
+        ii = _INPUTUNION()
+        ii.mi = MOUSEINPUT(dx_int, dy_int, 0, MOUSEEVENTF_MOVE, 0, ctypes.c_void_p(0))
+        command = INPUT(INPUT_MOUSE, ii)
+
+        result = SendInput(1, ctypes.byref(command), ctypes.sizeof(command))
+        if result == 0:
+            err = ctypes.get_last_error()
+            logger.error(f"SendInput mouse_move failed winerr {err}")
 
     def click(self, press: bool):
         if press == self.is_clicking:
             return
 
         flag = MOUSEEVENTF_LEFTDOWN if press else MOUSEEVENTF_LEFTUP
-        ii = InputUnion()
-        ii.mi = MouseInput(0, 0, 0, flag, 0, ctypes.pointer(ctypes.c_ulong(0)))
-        command = Input(ctypes.c_ulong(0), ii)
+        ii = _INPUTUNION()
+        ii.mi = MOUSEINPUT(0, 0, 0, flag, 0, ctypes.c_void_p(0))
+        command = INPUT(INPUT_MOUSE, ii)
         SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
 
-        self.is_clicking = press
-        logger.debug(f"Mouse l-click {'PRESSED' if press else 'RELEASED'}")
+        result = SendInput(1, ctypes.byref(command), ctypes.sizeof(command))
+        if result == 0:
+            err = ctypes.get_last_error()
+            logger.error(f"SendInput mouse_click failed winerr {err}")
+        else:
+            self.is_clicking = press
